e494075205430c21dbf5bfc44c1abdde73fb9045
甄士刚 <zhenshigang@xdf.cn>
2021-11-29 16:34:58 +0800

    连接数据库

--- expressTemplate/routes/index.js
+++ expressTemplate/routes/index.js
@@ -1,9 +1,19 @@
+const { json } = require('express');
 var express = require('express');
 var router = express.Router();
-
+const {querySql} = require('../db')
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  querySql(`select * from db`).then((result)=>{
+    res.render('index', {
+      title: JSON.stringify(result)
+    });
+  }).catch((err)=>{
+    res.render('index', {
+      title: err
+    });
+  })
+ 
 });
 
 module.exports = router;
