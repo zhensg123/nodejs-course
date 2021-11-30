9126bc5899471c5736599cd05e858e83c85e9c66
甄士刚 <zhenshigang@xdf.cn>
2021-11-30 19:57:13 +0800

    练习

--- expressTemplate/routes/index.js
+++ expressTemplate/routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 const {querySql} = require('../db')
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log(process.env.NODE_ENV)
   querySql(`select * from db`).then((result)=>{
     res.render('index', {
       title: JSON.stringify(result)
